6dec3a8f88b4f610f07ca7f7ed4f1c698154390d
Yasser <yasser@khancodes.com>
2020-05-21 15:47:34 -0700

    Added normalize.css file
    Added 5 quotes and normalize.css file

--- js/script.js
+++ js/script.js
@@ -4,13 +4,40 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+      {
+        quote: "Yesterday I was clever, so I wanted t ochange the world. Today I am wise, so I am changing myself",
+        source: "Jalaluddin Rumi",
+      },
+      {
+        quote: "When you forget that you need God, he puts you in a situation that causes you to call upon him, and that's for your own good",
+        source: "Omar Suleiman",
+      },
+      {
+        quote: "They plan, and God plans. Surely, God is the best of planners",
+        source: "God",
+        citation: "Quran 5:6",
+      },
+      {
+        quote: "We will never surrender. We win or we die. And don't think it stops there. You will have the next generation to fight; and after the next, the next. As for me, I will live longer than my hangman.",
+        source: "Omar Mukhtar",
+        citation: "Lion of the Desert",
+        year: 1980
+      },
 
+      {
+        quote: "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion'",
+        source: "Muhammad Ali",
+        
+
+      }
+];
 
 
 /***
